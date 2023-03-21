ea83c41955b6eba9fdbe8f7d3d587c259fc72d0c
Lucas Minter <lsminteris@gmail.com>
2023-03-20 21:39:48 -0700

    youtube fetch working, exceeded quota

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   reactStrictMode: true,
-  env: {
+  serverRuntimeConfig: {
     API_KEY: process.env.NEXT_PUBLIC_API_KEY,
     CHANNEL_ID: process.env.NEXT_PUBLIC_CHANNEL_ID
   }
--- src/components/YouTubeVideo.js
+++ src/components/YouTubeVideo.js
@@ -4,29 +4,21 @@ import axios from 'axios';
 export default function YouTubeVideo() {
   const [videoData, setVideoData] = useState(null)
 
-  useEffect(() => {
-    async function fetchVideoData() {
-      try {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-          params: {
-            part: 'snippet',
-            channelId: process.env.NEXT_PUBLIC_CHANNEL_ID,
-            maxResults: 1,
-            order: 'date',
-            type: 'video',
-            key: process.env.NEXT_PUBLIC_API_KEY,
-          },
-        });
-        if (response.data.items && response.data.items.length > 0) {
-          setVideoData(response.data.items[0]);
-        }
-      } catch (error) {
-        console.error(error);
+  async function fetchVideoData() {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.NEXT_PUBLIC_CHANNEL_ID}&maxResults=1&order=date&type=video&key=${process.env.NEXT_PUBLIC_API_KEY}`
+      );
+      const data = await response.json();
+      if (data.items && data.items.length > 0) {
+        setVideoData(data.items[0]);
       }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    fetchVideoData();
-  });
+  fetchVideoData();
 
 
   return (

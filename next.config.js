30ac6f4825b0fec146a00f4cbea27b95f4c7b972
Lucas Minter <lsminteris@gmail.com>
2023-03-25 11:19:49 -0700

    testing removing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
 
-module.exports = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-    CHANNEL_ID: process.env.NEXT_PUBLIC_CHANNEL_ID
-  }
-}
+// module.exports = {
+//   reactStrictMode: true,
+//   serverRuntimeConfig: {
+//     API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+//     CHANNEL_ID: process.env.NEXT_PUBLIC_CHANNEL_ID
+//   }
+// }

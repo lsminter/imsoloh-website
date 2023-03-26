9d17f160b2749520258476d9e8a2f1a19c036b38
Lucas Minter <lsminteris@gmail.com>
2023-03-26 12:10:11 -0700

    set global text color to white

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import Footer from '../components/Footer.js'
 export default function App({ Component, pageProps }) {
   return (
     // fix bottom of image size, need to still do the fade to black
-    <div className="h-screen bg-center bg-cover bg-no-repeat bg-hero">
+    <div className="h-screen text-white bg-center bg-cover bg-no-repeat bg-hero">
       <Layout>
         <Component {...pageProps} />
       <Footer />

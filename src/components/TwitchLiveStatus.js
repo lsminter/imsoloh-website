27dbfb5a0bc570dd15996bbf3e80b2bcc9b15687
Lucas Minter <lsminteris@gmail.com>
2023-04-14 13:01:34 -0700

    fixed is live component to show if streamer is live

--- src/components/TwitchLiveStatus.js
+++ src/components/TwitchLiveStatus.js
@@ -6,13 +6,19 @@ const TwitchLiveStatus = () => {
 
   useEffect(() => {
     const fetchStream = async () => {
-      const response = await axios.get(`https://api.twitch.tv/helix/streams?user_id=${process.env.NEXT_PUBLIC_SOLOH_ID}`, {
+      const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_CLIENT_SECRET}&grant_type=client_credentials`)
+      const bearerToken = response.data.access_token
+      const getStream = await axios.get(`https://api.twitch.tv/helix/streams?user_login=soloh`, {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`, 
+          Authorization: `Bearer ${bearerToken}`, 
           'Client-ID': process.env.NEXT_PUBLIC_CLIENT_ID,
-        },
+        }
       })
-      setIsLive(response.data.data.length > 0);
+      try {
+        setIsLive(getStream.data.data[0].type);
+      } catch {
+        setIsLive("false")
+      }
     }
     
     fetchStream()
@@ -20,7 +26,7 @@ const TwitchLiveStatus = () => {
     
   return (
     <div className="flex justify-center items-center flex-1 max-w-md bg-twitch-purple h-16">
-      {isLive
+      {isLive === "live"
       ? (
         <a href="https://www.twitch.tv/imsoloh" className="flex-1 text-center">
           <span className="text-white text-xl">

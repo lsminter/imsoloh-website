744a60452bc0b69a051ac83550fd7659c07fa5bc
Lucas Minter <lsminteris@gmail.com>
2023-04-14 12:01:50 -0700

    moved background to local file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export default function Layout ({ children }){
           playsInline
           className="min-w-full min-h-full absolute object-cover"
         >
-          <source src="https://res.cloudinary.com/dgjsveger/video/upload/v1679344768/background-video_ivcz7m.mp4" type="video/mp4"/>
+          <source src="/images/background-video.mp4" type="video/mp4"/>
         </video>
       </div>
       <div className="container mx-auto px-4 z-20">
